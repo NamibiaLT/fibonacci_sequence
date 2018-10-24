55331c6f043f3843f5ecbf73e08399e3d1479896
NLT <NamibiaLT@users.noreply.github.com>
2018-10-24 12:57:23 -0700

    attempt at second solution

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,6 +1,12 @@
 class Fibonacci
   def self.fibonacci_generator(max)
     (1..max).inject([1,2]) { |fib_array| fib_array << fib_array.last(2).inject(:+) }
+
+    # (1..max).inject([1,2]) do |fib_array|
+    #   fib_array << fib_array.last(2).inject(:+)
+    #   break if fib_array.last == max
+    # end
+    # ------------------ #
     # inject the first two numbers of your fib seq in an array
     # then pass a block and inject
     # the sum of the last two numbers in your seq
