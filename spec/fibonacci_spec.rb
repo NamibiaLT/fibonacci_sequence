17f756fbf00de93ab6504fbab601c51205df9cc9
NLT <NamibiaLT@users.noreply.github.com>
2018-10-24 11:35:17 -0700

    add tests for fibonacci_generator method

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,2 +1,14 @@
 require 'fibonacci'
 require 'rspec'
+
+RSpec.describe 'Fibonacci' do
+  desribe 'fibonacci_generator' do
+    it 'should return a fibonacci sequence given 5' do
+      expect(Fibonacci.fibonacci_generator(5)).to eq([1,2,3,5])
+    end
+
+    it 'should return a fibonacci sequence given 610' do
+      expect(Fibonacci.fibonacci_generator(610)).to eq([1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610])
+    end
+  end
+end
